b507dfd024a71d26d6361299203fb8139bd7048a
Varun Sudrik <varunsudrik99@gmail.com>
2023-03-26 15:58:48 +0530

    commit

--- script.js
+++ script.js
@@ -1,41 +1,169 @@
-function clock() {
-  var ampm;
-  let hour = document.querySelector(".hour");
-  let minute = document.querySelector(".minute");
-  let period = document.querySelector(".zone");
+//Initial References
+let timerRef = document.querySelector(".timer-display");
+const hourInput = document.getElementById("hourInput");
+const minuteInput = document.getElementById("minuteInput");
+const secondInput = document.getElementById("secondInput");
+const activeAlarms = document.querySelector(".activeAlarms");
+const setAlarm = document.getElementById("set");
+let alarmsArray = [];
+let alarmSound = new Audio("./alarm.mp3");
 
-  let min = new Date().getMinutes();
-  let h = new Date().getHours();
+let initialHour = 0,
+  initialMinute = 0,
+  initialSecond = 0,
+  alarmIndex = 0;
 
-  ampm = h >= 12 ? "PM" : "AM";
+//Append zeroes for single digit
+const appendZero = (value) => (value < 10 ? "0" + value : value);
 
-  h = h < 10 ? "0" + h : h;
-  min = min < 10 ? "0" + min : min;
+//Search for value in object
+const searchObject = (parameter, value) => {
+  let alarmObject,
+    objIndex,
+    exists = false;
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm[parameter] == value) {
+      exists = true;
+      alarmObject = alarm;
+      objIndex = index;
+      return false;
+    }
+  });
+  return [exists, alarmObject, objIndex];
+};
 
-  h = h > 12 ? h - 12 : h;
-  period.textContent = ampm;
-  hour.textContent = h;
-  minute.textContent = min;
+//Display Time
+function displayTimer() {
+  let date = new Date();
+  let [hours, minutes, seconds] = [
+    appendZero(date.getHours()),
+    appendZero(date.getMinutes()),
+    appendZero(date.getSeconds()),
+  ];
+
+  //Display time
+  timerRef.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+  //Alarm
+  alarmsArray.forEach((alarm, index) => {
+    if (alarm.isActive) {
+      if (
+        `${alarm.alarmHour}:${alarm.alarmMinute}:${alarm.alarmSecond}` ===
+        `${hours}:${minutes}:${seconds}`
+      ) {
+        alarmSound.play();
+        alarmSound.loop = true;
+      }
+    }
+  });
 }
-setInterval(clock, 1000);
 
-let selectMenue = document.querySelectorAll("select");
-for (let i = 12; i > 0; i--) {
-  if (i < 10) {
-    i = "0" + i;
+const inputCheck = (inputValue) => {
+  inputValue = parseInt(inputValue);
+  if (inputValue < 10) {
+    inputValue = appendZero(inputValue);
   }
-  let option = `<option value="${i}">${i}</option>`;
-  selectMenue[0].firstElementChild.insertAdjacentHTML("afterend", option);
-}
-for (let i = 60; i > 0; i--) {
-  if (i < 10) {
-    i = "0" + i;
+  return inputValue;
+};
+
+hourInput.addEventListener("input", () => {
+  hourInput.value = inputCheck(hourInput.value);
+});
+
+minuteInput.addEventListener("input", () => {
+  minuteInput.value = inputCheck(minuteInput.value);
+});
+secondInput.addEventListener("input", () => {
+  secondInput.value = inputCheck(secondInput.value);
+});
+
+//Create alarm div
+
+const createAlarm = (alarmObj) => {
+  //Keys from object
+  const { id, alarmHour, alarmMinute, alarmSecond } = alarmObj;
+  //Alarm div
+  let alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarm");
+  alarmDiv.setAttribute("data-id", id);
+  alarmDiv.innerHTML = `<span>${alarmHour}: ${alarmMinute}: ${alarmSecond}</span>`;
+
+  //checkbox
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      startAlarm(e);
+    } else {
+      stopAlarm(e);
+    }
+  });
+  alarmDiv.appendChild(checkbox);
+  //Delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add("deleteButton");
+  deleteButton.addEventListener("click", (e) => deleteAlarm(e));
+  alarmDiv.appendChild(deleteButton);
+  activeAlarms.appendChild(alarmDiv);
+};
+
+//Set Alarm
+setAlarm.addEventListener("click", () => {
+  alarmIndex += 1;
+
+  //alarmObject
+  let alarmObj = {};
+  alarmObj.id = `${alarmIndex}_${hourInput.value}_${minuteInput.value}_${secondInput.value}`;
+  alarmObj.alarmHour = hourInput.value;
+  alarmObj.alarmMinute = minuteInput.value;
+  alarmObj.alarmSecond = secondInput.value;
+  alarmObj.isActive = false;
+  console.log(alarmObj);
+  alarmsArray.push(alarmObj);
+  createAlarm(alarmObj);
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+  secondInput.value = appendZero(initialSecond);
+});
+
+//Start Alarm
+const startAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = true;
   }
-  let option = `<option value="${i}">${i}</option>`;
-  selectMenue[1].firstElementChild.insertAdjacentHTML("afterend", option);
-}
-for (let i = 2; i > 0; i--) {
-  let ampm = i == 1 ? "AM" : "PM";
-  let option = `<option value="${i}">${i}</option>`;
-  selectMenue[2].firstElementChild.insertAdjacentHTML("afterend", option);
-}
+};
+
+//Stop alarm
+const stopAlarm = (e) => {
+  let searchId = e.target.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    alarmsArray[index].isActive = false;
+    alarmSound.pause();
+  }
+};
+
+//delete alarm
+const deleteAlarm = (e) => {
+  let searchId = e.target.parentElement.parentElement.getAttribute("data-id");
+  let [exists, obj, index] = searchObject("id", searchId);
+  if (exists) {
+    e.target.parentElement.parentElement.remove();
+    alarmsArray.splice(index, 1);
+  }
+};
+
+window.onload = () => {
+  setInterval(displayTimer);
+  initialHour = 0;
+  initialMinute = 0;
+  initialSecond = 0;
+  alarmIndex = 0;
+  alarmsArray = [];
+  hourInput.value = appendZero(initialHour);
+  minuteInput.value = appendZero(initialMinute);
+  secondInput.value = appendZero(initialSecond);
+};
